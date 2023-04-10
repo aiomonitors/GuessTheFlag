d517b6eca98427d529e374be3d3dad5c577b3c70
aiomonitors <sachow168@gmail.com>
2023-04-10 18:49:42 -0400

    Create GuessTheFlagGame struct

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -7,13 +7,34 @@
 
 import SwiftUI
 
+var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+
+struct GuessTheFlagGame {
+    let correctAnswer = Int.random(in: 0...2)
+    var randomCountries = Set<String>()
+    
+    init() {
+        while randomCountries.count < 3 {
+            let randomCountry = countries.randomElement()
+            
+            guard let randomCountry = randomCountry else {
+                continue
+            }
+            
+            if !randomCountries.contains(randomCountry) {
+                randomCountries.insert(randomCountry)
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    var correctAnswer = Int.random(in: 0...countries.count)
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Tap the flag of")
+            Text(countries[correctAnswer])
         }
         .padding()
     }
